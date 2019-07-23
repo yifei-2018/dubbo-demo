94dc471c807b2d1b9cacb9ec95fd1d1465d7baf7
luqs <luqingsong@qmgf.net>
2019-07-23 16:30:02 +0800

    脚本修改

--- dubbo-demo-provider/src/main/assembly/bin/start.sh
+++ dubbo-demo-provider/src/main/assembly/bin/start.sh
@@ -51,9 +51,9 @@ fi
 JAVA_MEM_OPTS=""
 BITS=`java -version 2>&1 | grep -i 64-bit`
 if [ -n "${BITS}" ]; then
-        JAVA_MEM_OPTS=" -server -Xmx6g -Xms2g -Xmn256m -XX:PermSize=128m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
+        JAVA_MEM_OPTS=" -server -Xmx2g -Xms2g -Xmn256m -XX:PermSize=128m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
     else
-        JAVA_MEM_OPTS=" -server -Xms6g -Xmx2g -XX:PermSize=128m -XX:SurvivorRatio=2 -XX:+UseParallelGC "
+        JAVA_MEM_OPTS=" -server -Xmx1g -Xms1g -XX:PermSize=128m -XX:SurvivorRatio=2 -XX:+UseParallelGC "
 fi
 
 # 运行启动类
